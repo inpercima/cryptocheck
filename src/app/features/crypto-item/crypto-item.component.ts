1a6aea240fc192e0d9e2ced6e134197f6d25d6ac
Toubsh <Toubsh@hotmail.de>
2020-02-12 02:24:34 +0100

    Display profit/loss for individual items (fix #4)
    * Create styling for profit (green), loss (red) and zero (black) on individual item

--- src/app/features/crypto-item/crypto-item.component.ts
+++ src/app/features/crypto-item/crypto-item.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+
+import { CryptoService } from '../dashboard/crypto.service';
 import { CryptoItem } from './crypto-item.model';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'cc-crypto-item',
   templateUrl: './crypto-item.component.html',
   styleUrls: ['./crypto-item.component.css'],
 })
-export class CryptoItemComponent implements OnInit {
+export class CryptoItemComponent implements OnInit, OnDestroy {
+
+  cryptoPrices: any;
+
+  subscription: Subscription;
 
   coin: string;
   amount: number;
   purchasePrice: number;
 
+  @Input() currency;
   @Input() item: CryptoItem;
+
   @Output() updateItem = new EventEmitter<CryptoItem>();
   @Output() removeItem = new EventEmitter<CryptoItem>();
 
-  constructor() { }
+  constructor(private cryptoService: CryptoService) { }
 
   ngOnInit() {
     this.coin = this.item.coin;
     this.amount = this.item.amount;
     this.purchasePrice = this.item.purchasePrice;
+    this.subscription = interval(100).subscribe(() => this.updatePrice());
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  updatePrice() {
+    this.cryptoService.price().subscribe(cryptoPrices => this.cryptoPrices = cryptoPrices);
   }
 
   changeItem(): void {
@@ -33,6 +51,14 @@ export class CryptoItemComponent implements OnInit {
 
   remove() {
     this.removeItem.emit(this.item);
+  }
+
+  profitLoss(): number {
+    return (this.cryptoPrices[this.item.coin + this.currency].value * this.item.amount) - (this.item.amount * this.item.purchasePrice);
+  }
+
+  conditionalColor() {
+    return this.profitLoss() < 0 ? { 'color': 'red' } : this.profitLoss() > 0 ? { 'color': 'green' } : { 'color': 'black' };
   }
 
 }
