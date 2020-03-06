4e485e365b0fe0bce10d6e35740651db5e9b63ab
inpercima <inpercima@gmail.com>
2020-03-06 17:17:58 +0100

    Remove unnecessary subscription to update and get price b/c the alue is in the service

--- src/app/features/crypto-item/crypto-item.component.ts
+++ src/app/features/crypto-item/crypto-item.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { CryptoService } from '../dashboard/crypto.service';
 import { CryptoItem } from './crypto-item.model';
-import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'cc-crypto-item',
   templateUrl: './crypto-item.component.html',
   styleUrls: ['./crypto-item.component.css'],
 })
-export class CryptoItemComponent implements OnInit, OnDestroy {
-
-  cryptoPrices: any;
-
-  subscription: Subscription;
+export class CryptoItemComponent implements OnInit {
 
   coin: string;
   amount: number;
@@ -31,15 +26,6 @@ export class CryptoItemComponent implements OnInit, OnDestroy {
     this.coin = this.item.coin;
     this.amount = this.item.amount;
     this.purchasePrice = this.item.purchasePrice;
-    this.subscription = interval(100).subscribe(() => this.updatePrice());
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-  updatePrice() {
-    this.cryptoService.price().subscribe(cryptoPrices => this.cryptoPrices = cryptoPrices);
   }
 
   changeItem(): void {
