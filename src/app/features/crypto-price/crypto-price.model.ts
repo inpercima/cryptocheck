dd772b35c51d5d49a578d12b158ab60e501d957f
inpercima <inpercima@gmail.com>
2020-03-06 16:52:30 +0100

    Create crypto-price model and set locale direct in service

---
+++ src/app/features/crypto-price/crypto-price.model.ts
@@ -0,0 +1,12 @@
+export interface CryptoPrice {
+
+  coin: string;
+
+  currency: string;
+
+  value: number;
+
+  trend: string;
+
+  locale: string;
+}
--- src/app/features/dashboard/crypto.service.ts
+++ src/app/features/dashboard/crypto.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 import { CryptoItem } from '../crypto-item/crypto-item.model';
+import { CryptoPrice } from '../crypto-price/crypto-price.model';
 
 import { Constants } from '../../core/constants';
 
@@ -18,28 +21,27 @@ export class CryptoService {
   price(): Observable<any> {
     return this.http.get<any>('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR').pipe(map(response => {
       const prices = {
-        BTCUSD: this.createPrice(response, 'BTC', 'USD'),
-        BTCEUR: this.createPrice(response, 'BTC', 'EUR'),
-        ETHUSD: this.createPrice(response, 'ETH', 'USD'),
-        ETHEUR: this.createPrice(response, 'ETH', 'EUR'),
+        BTCUSD: this.createPrice(response, 'BTC', 'USD', ''),
+        BTCEUR: this.createPrice(response, 'BTC', 'EUR', 'de-DE'),
+        ETHUSD: this.createPrice(response, 'ETH', 'USD', ''),
+        ETHEUR: this.createPrice(response, 'ETH', 'EUR', 'de-DE'),
       };
       this.cryptoPrices = response;
       return prices;
     }));
   }
 
-  createPrice(res: any, coin: string, currency: string): any {
+  createPrice(res: any, coin: string, currency: string, locale: string): CryptoPrice {
     let trend = Constants.TREND_NORMAL;
     const value = res[coin][currency];
     if (this.cryptoPrices) {
       trend = value > this.cryptoPrices[coin][currency] ? Constants.TREND_UP :
         (value < this.cryptoPrices[coin][currency] ? Constants.TREND_DOWN : trend);
     }
-    return { coin, currency, value, trend } as any;
+    return {coin, currency, value, trend, locale} as CryptoPrice;
   }
 
-  profitLoss(item: CryptoItem, cryptoPrice: any, currency: string) {
-    return (cryptoPrice[item.coin + currency].value * item.amount) - (item.amount * item.purchasePrice);
+  profitLoss(item: CryptoItem, cryptoPrices: any, currency: string): number {
+    return (cryptoPrices[item.coin + currency].value * item.amount) - (item.amount * item.purchasePrice);
   }
-
 }
