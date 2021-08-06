6a99ae32b8d3e1085b2708b66db22192a3fd29cd
inpercima <inpercima@gmail.com>
2021-08-06 14:36:37 +0200

    Add wallet controller as template

---
+++ server/src/main/java/net/inpercima/cryptocheck/web/rest/WalletController.java
@@ -0,0 +1,25 @@
+package net.inpercima.cryptocheck.web.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import net.inpercima.cryptocheck.service.RestService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/wallets")
+public class WalletController {
+
+    @Value("${app.bitpanda.apikey}")
+    private String bitpandaApiKey;
+
+    private final RestService restService;
+
+    @GetMapping
+    public Object listAll() {
+        return restService.getEntity("https://api.bitpanda.com/v1/wallets", bitpandaApiKey, Object.class);
+    }
+}
