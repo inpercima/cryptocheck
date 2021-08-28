753b80c556d810cabf5f67e77aa8d56cb0bf828a
inpercima <inpercima@gmail.com>
2021-08-28 14:29:17 +0200

    First integration of tax information list

--- server/src/main/java/net/inpercima/cryptocheck/repository/TransactionAssetRepository.java
+++ server/src/main/java/net/inpercima/cryptocheck/repository/TransactionAssetRepository.java
@@ -10,24 +10,24 @@ import net.inpercima.cryptocheck.entity.TransactionAsset;
 
 public interface TransactionAssetRepository extends TransactionRepository<TransactionAsset> {
 
-        @Query("SELECT t FROM TransactionAsset t WHERE t.type = 'sell' AND t.status = 'finished'")
-        public List<TransactionAsset> findAllFinishedSells();
+    @Query("SELECT t FROM TransactionAsset t WHERE t.type = 'sell' AND t.status = 'finished'")
+    public List<TransactionAsset> findAllFinishedSells();
 
-        @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
-                        + "WHERE t.type = 'buy' AND t.status = 'finished' AND ta.name = :assetSymbol AND t.number = :number")
-        public TransactionAsset findRelatedBuyTransaction(final String assetSymbol, final BigDecimal number);
+    @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
+            + "WHERE t.type = 'buy' AND t.status = 'finished' AND ta.name = :assetSymbol AND t.number = :number")
+    public TransactionAsset findRelatedBuyTransaction(final String assetSymbol, final BigDecimal number);
 
-        @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
-                        + "WHERE t.relationId IS NULL AND t.status = 'finished' "
-                        + "AND ta.name = :assetSymbol AND t.type IN ('buy', 'sell') ORDER BY ta.name, t.date, t.type")
-        public List<TransactionAsset> findAllUnrelatedTransactions(final String assetSymbol);
+    @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
+            + "WHERE t.relationId IS NULL AND t.status = 'finished' "
+            + "AND ta.name = :assetSymbol AND t.type IN ('buy', 'sell') ORDER BY ta.name, t.date, t.type")
+    public List<TransactionAsset> findAllUnrelatedTransactions(final String assetSymbol);
 
-        @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
-                        + "WHERE t.relationId IS NULL AND t.status = 'finished' ORDER BY ta.name, t.date, t.type")
-        public List<TransactionAsset> findAllUnrelatedTransactions();
+    @Query("SELECT t FROM TransactionAsset t LEFT JOIN t.typeAsset ta "
+            + "WHERE t.relationId IS NULL AND t.status = 'finished' ORDER BY ta.name, t.date, t.type")
+    public List<TransactionAsset> findAllUnrelatedTransactions();
 
-        @Query("SELECT t FROM TransactionAsset t "
-                        + "WHERE t.relationId IS NOT NULL AND t.date BETWEEN :startDate AND :endDate ORDER BY t.typeAsset, t.date, t.type")
-        public List<TransactionAsset> findAllRelatedTransactions(final LocalDateTime startDate,
-                        final LocalDateTime endDate);
+    @Query("SELECT t FROM TransactionAsset t "
+            + "WHERE t.relationId IS NOT NULL AND t.date BETWEEN :startDate AND :endDate ORDER BY t.typeAsset, t.date, t.type")
+    public List<TransactionAsset> findAllRelatedTransactions(final LocalDateTime startDate,
+            final LocalDateTime endDate);
 }
